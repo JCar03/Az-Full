f9bf9181efda4176513638de61f76e2ab1c83800
JCar03 <102256453+JCar03@users.noreply.github.com>
2022-03-25 00:57:09 -0500

    Update SpringBootEcommerceApplication.java

--- backend/src/main/java/com/ecom/ecommerce/SpringBootEcommerceApplication.java
+++ backend/src/main/java/com/ecom/ecommerce/SpringBootEcommerceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class pringBootEcommerceApplication {
+public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
