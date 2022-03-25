9d66643c2c8e8560d66088ec5b8ab702f6b7474a
JCas03 <03juan.c.castorena@gmail.com>
2022-03-24 22:55:45 -0500

    a

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -1,10 +1,9 @@
 export default {
 
     oidc: {
-        clientId: '0oa4c8dn9p65fmC7O5d7',
-        issuer: 'https://dev-5165573.okta.com/oauth2/default',
-        redirectUri: 'https://my-ecommerce-app.azurewebsites.net/login/callback',
-        //redirectUri: 'http://localhost:4200/login/callback',
+        clientId: '0oa4ccnklyI9a8zE55d7',
+        issuer: 'https://dev-2815983.okta.com/oauth2/default',
+        redirectUri: 'https://az-frt.azurewebsites.net/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  luv2shopApiUrl: "http://localhost:8080/api",
-  stripePublishableKey: "pk_test_51KgZckE6okuwhGCfjABpYUUcNI0kbbK36WsnseXatx6xdoSKMsF7hO6mSLHDvNocKnQYdeMI9bwbBCXJpiIKMbM3002aJHIZkX"
+  luv2shopApiUrl: "https://bkend.azurewebsites.net/api",
+  stripePublishableKey: "<<ADD-YOUR-STRIPE-PUBLISHABLE-KEY>>"
 };
 
 /*
